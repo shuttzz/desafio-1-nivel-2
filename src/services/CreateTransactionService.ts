7ac29cd679976632a688f7ff563e272b45c18347
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2020-06-25 20:40:59 -0300

    Desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,18 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Request): Transaction {
+    if (!['outcome', 'income'].includes(type)) {
+      throw new Error('The type must be equal to income and outcome');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === "outcome" && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+
+    return this.transactionsRepository.create({title, value, type});
   }
 }
 
